73042c9b58c6efae41c996c8176bfe2ddcf6061c
xXccoleXx <71529472+xXccoleXx@users.noreply.github.com>
2023-07-19 13:51:55 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 document.getElementById('submitButton').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent form submission to avoid page reload
+    uploadFile();
+});
 
+async function uploadFile() {
     const imageInput = document.getElementById('file-input');
 
     if (imageInput.files.length === 0) {
@@ -11,27 +14,21 @@ document.getElementById('submitButton').addEventListener('click', function(event
     const file = imageInput.files[0];
     const formData = new FormData();
     formData.append('image', file);
-    // alert(file.name + ' ' + file.imageInput);
-    
 
-    const response = fetch('http://54.211.46.55:5000/upload', {
-        // mode: 'no-cors',
-        method: 'POST',
-        body: formData,
-    })
-    .then(response => {
+    try {
+        const response = await fetch('http://54.211.46.55:5000/upload', {
+            method: 'POST',
+            body: formData,
+        });
+
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Upload failed');
         }
-        return response.json();
-        // return response.text();
-    })
-    .then(data => {
-        console.log(data); // Log the JSON response to the console
-        alert('Uploaded file name: ' + file.name + '\n' + data.message);
-    })
-    .catch(error => {
-        console.log(data); 
-        alert('Error: ' + error.message);
-    });
-});
+
+        const fileName = await response.text();
+        console.log('Uploaded file name:', fileName);
+        alert('File uploaded. Server response: ' + fileName);
+    } catch (error) {
+        console.error('Error during file upload:', error);
+    }
+}
