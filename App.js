5bf8cecb2b3ce45a98d6043d25cb622f91041aab
duderock4545 <121646668+duderock4545@users.noreply.github.com>
2023-07-20 14:52:56 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-document.getElementById('submitButton').addEventListener('click', function(event) {
+document.getElementById('submitButton').addEventListener('click', function (event) {
     event.preventDefault(); // Prevent form submission to avoid page reload
     uploadFile();
 });
@@ -25,9 +25,17 @@ async function uploadFile() {
             throw new Error('Upload failed');
         }
 
-        const fileName = await response.text();
-        console.log('Uploaded file name:', fileName);
-        alert('File uploaded. Server response: ' + fileName);
+        const scrambledImgName = await response.text(); // Assuming the response is the image name (e.g., "12345678.png")
+        const scrambledImgUrl = `https://s3.amazonaws.com/scrambledlikesumeggs/${scrambledImgName}`;
+
+        // Display the scrambled image on the page
+        const scrambledImg = document.createElement('img');
+        scrambledImg.src = scrambledImgUrl;
+        const scrambledDiv = document.getElementById('scrambled');
+        scrambledDiv.innerHTML = ''; // Clear any existing content
+        scrambledDiv.appendChild(scrambledImg);
+
+        // alert('File uploaded. Scrambled image URL: ' + scrambledImgUrl);
     } catch (error) {
         console.error('Error during file upload:', error);
     }
